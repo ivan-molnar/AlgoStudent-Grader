15d8f4f5030af5107eecf2b668d1b49049028d3d
Molnar Ivan <80251702+ivan-molnar@users.noreply.github.com>
2022-11-28 21:20:13 +0100

    Updated Activity model

--- Grader/Grader/Models/Activity.cs
+++ Grader/Grader/Models/Activity.cs
@@ -4,5 +4,42 @@
     {
         public string Name { get; set; }
         public List<Category> Categories { get; set; }
+
+        // Self calculated from here
+        public int TotalPoints = 0;
+        public int OwnedPoints = 0;
+        public int LostPoints = 0;
+        public int UndecidedPoints = 0;
+
+        public void init()
+        {
+            foreach (var category in Categories)
+            {
+                category.init();
+
+                TotalPoints += category.MaxPoints;
+
+                if(category.Grades.Count == 0) { this.UndecidedPoints += category.MaxPoints; }
+                else
+                {
+                    OwnedPoints += category.Grade;
+                    LostPoints += category.MaxPoints - category.Grade;
+                }
+            }
+        }
+
+        public void addGrade(string name, int grade)
+        {
+            string actId = this.Name.Substring(0, this.Name.IndexOf(" "));
+            string gradeId = name.Substring(0, name.IndexOf("-"));
+
+            if(actId == gradeId)
+            {
+                this.Categories
+                    .Where(categ => categ.Name == name)
+                    .ToList()
+                    .ForEach(categ => categ.Grades.Add(grade));
+            }
+        }
     }
 }
