afcff90e202957f99cc23ad76dc8dba039c74994
Molnar Ivan <80251702+ivan-molnar@users.noreply.github.com>
2022-11-29 09:32:21 +0100

    Fixed grade update

--- Grader/Grader/Models/Activity.cs
+++ Grader/Grader/Models/Activity.cs
@@ -22,8 +22,8 @@
                 if(category.Grades.Count == 0) { this.UndecidedPoints += category.MaxPoints; }
                 else
                 {
-                    OwnedPoints += category.Grade;
-                    LostPoints += category.MaxPoints - category.Grade;
+                    this.OwnedPoints += category.Grade;
+                    this.LostPoints += category.MaxPoints - category.Grade;
                 }
             }
         }
@@ -33,10 +33,10 @@
             string actId = this.Name.Substring(0, this.Name.IndexOf(" "));
             string gradeId = name.Substring(0, name.IndexOf("-"));
 
-            if(actId == gradeId)
+            if(actId == gradeId)    // Définir does not go to its place
             {
                 this.Categories
-                    .Where(categ => categ.Name == name)
+                    .Where(categ => String.Equals(categ.Name.Substring(0, categ.Name.IndexOf(" ")), name.Substring(0, name.IndexOf(" ")), StringComparison.Ordinal))
                     .ToList()
                     .ForEach(categ => categ.Grades.Add(grade));
             }
